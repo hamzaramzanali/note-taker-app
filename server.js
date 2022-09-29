8335c778d8a8cc7c16ba58c26178992b214908bd
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-09-29 16:50:42 -0500

    added code in server.js to allow notes to save and delete

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const db = require('./db/db.json');
+const { json } = require('express');
 
 const PORT = process.env.PORT || 3001;
 
@@ -15,6 +18,55 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+app.get('/api/notes', (req, res) =>
+  res.json(db.slice(1))
+);
+
+app.post('/api/notes', (req, res) => {
+  const newNote = createNote(req.body, db)
+  res.json(newNote)
+});
+
+const createNote = (body, notesArray) => {
+  const newNote = body
+  if (!Array.isArray(notesArray)) {
+    notesArray = []
+  }
+  if (notesArray.length === 0) {
+    notesArray.push(0)
+  }
+  body.id = notesArray.length
+  notesArray[0]++
+  notesArray.push(newNote)
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'), 
+    JSON.stringify(notesArray, null, 2)
+  )
+  return newNote;
+}
+
+app.delete('/api/notes/:id', (req, res) =>{
+  deleteNote(req.params.id, db)
+  res.json(true)
+})
+
+const deleteNote = (id, notesArray) => {
+  for (let i = 0; i < notesArray.length; i++) {
+    let note = notesArray[i]
+    if (note.id == id) {
+      notesArray.splice(i, 1)
+      fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+      )
+      break;
+    }
+  }
+}
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
